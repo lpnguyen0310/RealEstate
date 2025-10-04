4ba5adbbe62e97a015171701e8b942e27cedb2a7
Trần Trọng Nhân-21095871 <nhanmpt000@gmail.com>
2025-10-04 15:40:29 +0700

    create footer

--- FE_Real-Estate/src/App.jsx
+++ FE_Real-Estate/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './layouts/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <p className='font-black'>
         Hello world ok
       </p>
+      <Footer />
     </>
   )
 }
